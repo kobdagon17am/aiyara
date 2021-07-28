47a94269b304ed60d7d332a095978d6eaf2e4be1
Sasanapong <sassanapong.p@gmail.com>
2021-07-29 02:52:44 +0700

    rename

--- local/app/Models/Frontend/location.php
+++ local/app/Models/Frontend/location.php
@@ -7,7 +7,7 @@ use DB;
 class Location extends Model
 {
 	public static function location($zon,$lang_id){
-        //te
+        //tecc
         $data = DB::table('dataset_business_location')
         ->select('*')
         ->leftjoin('branchs', 'dataset_business_location.id', '=', 'branchs.business_location_id_fk')
