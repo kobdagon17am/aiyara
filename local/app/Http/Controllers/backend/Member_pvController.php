38deb318164bb892d1936ad4c4ac04f399580786
Sasanapong <sassanapong.p@gmail.com>
2022-02-08 16:51:11 +0700

    registers

--- local/app/Http/Controllers/backend/Member_pvController.php
+++ local/app/Http/Controllers/backend/Member_pvController.php
@@ -116,8 +116,14 @@ class Member_pvController extends Controller
       } elseif($request->type == 'customer_address_card') {
         $this->updateAddressCardInformation($request, $customer_id);
         return back()->with(['alert' =>['status'=>'success', 'msg'=>'บันทึกข้อมูลที่อยู่ตามบัตรประชาชนสำเร็จ']]);
-      }else{
+      } elseif($request->type == 'address_send') {
+        $this->updateAddresSendInformation($request, $customer_id);
+        return back()->with(['alert' =>['status'=>'success', 'msg'=>'บันทึกข้อมูลที่อยู่จัดส่งสำเร็จ']]);
+      } elseif($request->type == 'bank') {
         $this->updateBankInformation($request, $customer_id);
+        return back()->with(['alert' =>['status'=>'success', 'msg'=>'บันทึกข้อมูลธนาคารสำเร็จ']]);
+      }else{
+        return back()->with(['alert' =>['status'=>'fail', 'msg'=>'การบันทึกข้อมูลไม่ถูกต้อง']]);
       }
 
     }
@@ -131,24 +137,26 @@ class Member_pvController extends Controller
 
     public function updateAddressCardInformation($request, $customer_id)
     {
-      DB::table('customers_address_card')->updateOrInsert([
+      $rs = DB::table('customers_address_card')->updateOrInsert([
         'customer_id' => $customer_id
       ],$request->except(['_method', '_token','type']));
+
 
      // $request->session()->flash('status_bank', 'แก้ไขข้อมูลธนาคารเรียบร้อย');
     }
 
-    public function updateBankInformation($request, $customer_id)
+    public function updateAddresSendInformation($request, $customer_id)
     {
-      $data = collect($request)->filter(function ($req, $key) {
-        return str_contains($key, 'bank_');
-      })->toArray();
 
       DB::table('customers_detail')->updateOrInsert([
         'customer_id' => $customer_id
-      ], $data);
-
-      $request->session()->flash('status_bank', 'แก้ไขข้อมูลธนาคารเรียบร้อย');
+      ],$request->except(['_method', '_token','type']));
+    }
+    public function updateBankInformation($request, $customer_id)
+    {
+      DB::table('customers_detail')->updateOrInsert([
+        'customer_id' => $customer_id
+      ],$request->except(['_method', '_token','type']));
     }
 
 
--- local/resources/views/backend/member_pv/edit_partials/address_send.blade.php
+++ local/resources/views/backend/member_pv/edit_partials/address_send.blade.php
@@ -5,8 +5,9 @@
         <h5 class="font-weight-bold">ที่อยู่จัดส่ง</h5>
         <hr class="mt-1 mb-2">
 
-        <form action="#" method="POST">
+        <form action="{{ route('backend.member_pv.update', $customer->id) }}" method="POST">
           @csrf
+          @method('PATCH')
 
           <div class="text-left">
             <div class="row">
@@ -43,7 +44,7 @@
               <div class="col-md-3">
                 <div class="form-group">
                   <label for="province">จังหวัด</label>
-                  <select name="province" id="province" class="form-control select2-templating select2-province" data-prefix="send" data-send-province>
+                  <select name="province_id_fk" id="province" class="form-control select2-templating select2-province" data-prefix="send" data-send-province>
                     <option value="">- เลือกจังหวัด -</option>
                     @foreach ($provinces as $provinceId => $provinceName)
                         <option value="{{ $provinceId }}" @if($provinceId == $customer->province_id_fk) selected @endif>{{ $provinceName }}</option>
@@ -54,13 +55,13 @@
               <div class="col-md-3">
                 <div class="form-group">
                   <label for="district">เขต/อำเภอ</label>
-                  <select name="district" id="district" class="form-control select2-templating select2-district" data-prefix="send" data-send-district></select>
+                  <select name="amphures_id_fk" id="district" class="form-control select2-templating select2-district" data-prefix="send" data-send-district></select>
                 </div>
               </div>
               <div class="col-md-3">
                 <div class="form-group">
                   <label for="sub_district">แขวง/ตำบล</label>
-                  <select name="sub_district" id="sub_district" class="form-control select2-templating select2-sub-district" data-prefix="send" data-send-sub-district></select>
+                  <select name="district_id_fk" id="sub_district" class="form-control select2-templating select2-sub-district" data-prefix="send" data-send-sub-district></select>
                 </div>
               </div>
               <div class="col-md-3">
@@ -69,14 +70,28 @@
                   <input type="text" class="form-control" name="zipcode" data-send-zip-code>
                 </div>
               </div>
+
+              <div class="col-md-2">
+                <div class="form-group">
+                  <label for="tel_mobile">เบอร์มือถือ</label>
+                  <input type="text" class="form-control" name="tel_mobile" value="{{ $customer->tel_mobile ?? '' }}">
+                </div>
+              </div>
+
+              <div class="col-md-2">
+                <div class="form-group">
+                  <label for="tel_home">เบอร์บ้าน</label>
+                  <input type="text" class="form-control" name="tel_home" value="{{ $customer->tel_home ?? '' }}">
+                </div>
+              </div>
             </div>
 
             <div class="text-right">
-              <button type="submit" class="btn btn-info">แก้ไขข้อมูล</button>
+              <button type="submit" name="type" value="address_send" class="btn btn-info">แก้ไขข้อมูล</button>
             </div>
-          </div> 
+          </div>
         </form>
-        
+
       </div>
     </div>
   </div>
