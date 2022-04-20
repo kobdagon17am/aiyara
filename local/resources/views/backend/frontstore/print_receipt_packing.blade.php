5a063102ae792d142670f017f5dc56f95cf945a5
warawut140 <warawut140@gmail.com>
2022-04-20 13:32:37 +0700

    แก้ชื่อมีเลข 0 เนื่องจากเกิดจาก prefix_name

--- local/resources/views/backend/frontstore/print_receipt_packing.blade.php
+++ local/resources/views/backend/frontstore/print_receipt_packing.blade.php
@@ -335,7 +335,9 @@ $value = DB::select("
                  $sRow = \App\Models\Backend\Frontstore::find($front_db->id);
                  $Delivery_location = DB::select(" select id,txt_desc from dataset_delivery_location  ");
                  $CusAddrFrontstore = \App\Models\Backend\CusAddrFrontstore::where('frontstore_id_fk',$front_db->id)->get();
-
+                 if(@$value[0]->prefix_name == 0){
+        @$value[0]->prefix_name = '';
+       }
                 echo "<b>ที่อยู่จัดส่ง</b>"."<br>";
                 echo "<b>".@$value[0]->prefix_name.@$value[0]->first_name.' '.@$value[0]->last_name."</b><br>";
 
