0fffcaa0088b83df0c8deefc75e1c830a13eb296
Sasanapong <sassanapong.p@gmail.com>
2023-01-14 11:43:04 +0700

    edit Pass

--- local/app/Http/Controllers/Frontend/EditPasswordController.php
+++ local/app/Http/Controllers/Frontend/EditPasswordController.php
@@ -16,32 +16,32 @@ class EditPasswordController extends Controller
 	}
 
 	public function edit_password_submit(Request $rs)
-	{ 
+	{
 		$user_password = Auth::guard('c_user')->user()->password;
-		$old_password = md5($rs->old_password);
+		// $old_password = md5($rs->old_password);
 		$confirm_edit_password = md5($rs->confirm_edit_password);
 
-		if($old_password != $user_password){
-			return redirect('chage_password')->withError('Old password is incorrect');
-		}else{
-			try {
+		// if($old_password != $user_password){
+		// 	return redirect('chage_password')->withError('Old password is incorrect');
+		// }else{
+			// try {
 				$update_pass = DB::table('customers')
 				->where('id', Auth::guard('c_user')->user()->id)
 				->update(['password' => $confirm_edit_password]);
 
 				return redirect('chage_password')->withSuccess('Edit Password Success');
-				
-			} catch (Exception $e) {
-				return redirect('chage_password')->withError($e);
-				
-			}
-			
 
-			
-		}
+			// } catch (Exception $e) {
+			// 	return redirect('chage_password')->withError($e);
+
+			// }
+
+
+
+		// }
 
 	}
-	
+
 
 
 }
