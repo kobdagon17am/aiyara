98535b8a0bf575ce05af0611329336b7674c20e0
warawut140 <warawut140@gmail.com>
2023-01-17 17:04:59 +0700

    ปรับแสดงปุ่ม import

--- local/resources/views/backend/pick_warehouse/qr_show.blade.php
+++ local/resources/views/backend/pick_warehouse/qr_show.blade.php
@@ -107,6 +107,7 @@
     <div class="row">
         <div class="col-md-12">
             รายการ QR CODE  <a href="javascript:;"  class="qr_scan_delete_all" oid="{{@$oid}}" pid="{{@$pid}}" style="color:red;"> <u> Remove All </u> </a>
+          @if(@\Auth::user()->id==29 || @\Auth::user()->id==1 || @\Auth::user()->id==111)
             <br><br>
             <form method="POST" action="{{ url('backend/qr_show_import') }}" enctype="multipart/form-data">
               @csrf
@@ -117,6 +118,7 @@
             <input type="hidden" name="p_list" value="{{@$p_list}}">
             <br><button onclick="return confirm('ยืนยันการทำรายการ?')" type="submit" class="btn btn-sm btn-primary">Import</button>
             </form>
+            @endif
             {{-- สินค้า {{$product->product_code}} : {{$product->product_name}} --}}
             <br><br>
             @foreach($qrs as $qr)
