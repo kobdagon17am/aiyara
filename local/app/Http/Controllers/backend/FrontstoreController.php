4b041d5d476166e69f0ca879f07dc1286a7049c0
warawut140 <warawut140@gmail.com>
2023-09-13 15:32:39 +0700

    เช็คสำหรับโอนเงินค่าจัดส่งทีหลัง

--- local/app/Http/Controllers/backend/FrontstoreController.php
+++ local/app/Http/Controllers/backend/FrontstoreController.php
@@ -1143,7 +1143,7 @@ class FrontstoreController extends Controller
         DB::table('db_orders')->where('code_order',request('bill_transfer_other'))->update([
           'delivery_location' => 4,
           'delivery_location_frontend' => $delivery_location_frontend,
-          'bill_transfer_other' => request('bill_transfer_other'),
+          'bill_transfer_other' => $sRow->code_order,
           'updated_at' => date('Y-m-d H:i:s'),
         ]);
 
